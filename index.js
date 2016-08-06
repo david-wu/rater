a27aeadd3ccd8a8d0d44a8de13654527aa5289ba
David Wu <dwu@mobileiron.com>
2016-08-05 17:28:04 -0700

    make better

--- index.js
+++ index.js
@@ -3,57 +3,75 @@
 var _ = require('lodash');
 
 function Rater(options){
-	_.defaults(this, {
-		factors: [],
-	});
 	_.extend(this, options);
-
-	this.factors.push({
-		weight: 1,
-		score: wordScore,
-	});
+	this.setDefaultFactors();
 }
 
 Rater.prototype = {
 
-	sorted: function(){
-		return this.array
-			.map(function(d){
-				return d.toLowerCase();
-			})
+	setDefaultFactors: function(){
+		this.factors = [
+			{
+				weight: 1,
+				score: Rater.wordScore.bind(this),
+			},
+			{
+				weight: 0.1,
+				score: Rater.wordLength.bind(this),
+			}
+		]
+	},
+
+	sorted: function(array){
+		return (array || this.array)
+			.map(this.transform.bind(this))
 			.map(this.process.bind(this))
 			.sort(function(a, b){
 				return b.totalScore-a.totalScore;
 			});
 	},
 
+	transform: function(d){
+		return d.toLowerCase();
+	},
+
 	process: function(d){
 		return {
 			d: d,
-			totalScore: this.factors.reduce((score, factor) => {
-				return score + factor.score(this.query.toLowerCase(), d);
+			totalScore: this.factors.reduce(function(score, factor){
+				return score + (factor.weight*factor.score(d));
 			}, 0),
 		};
 	},
 
 }
 
-function wordScore(query, word){
+
+Rater.wordScore = function(word){
 	var score = 0;
 	var queryIndex = 0;
 	var straightMatches = 0;
 
 	for(var i=0, l=word.length; i<l; i++){
-		if(word[i]===query[queryIndex]){
+		if(word[i]===this.query[queryIndex]){
+
 			straightMatches++;
 			score+=straightMatches;
+
+			var weightForMatchPosition = 1/(Math.abs(queryIndex-i)+1);
+			score+=weightForMatchPosition;
+
 			queryIndex++;
 		}else{
 			straightMatches=0;
 		}
 	}
 	return score;
-}
+};
+
+Rater.wordLength = function(word){
+	return -word.length;
+};
 
 
 if(module && module.exports){
